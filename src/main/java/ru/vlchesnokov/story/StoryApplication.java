857e7411481f84309ca36eb331a59c515359ec96
Vadim Chesnokov <vlchesnokov@bk.ru>
2022-03-10 07:35:32 +0300

    added SpringBoot application

---
+++ src/main/java/ru/vlchesnokov/story/StoryApplication.java
@@ -0,0 +1,16 @@
+package ru.vlchesnokov.story;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Точка входа в приложение
+ */
+@SpringBootApplication
+public class StoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryApplication.class, args);
+	}
+
+}
