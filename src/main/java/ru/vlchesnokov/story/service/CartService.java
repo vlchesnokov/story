d0efbdb3da695f55c1bb81b2b24d3ccf570d5994
Vadim Chesnokov <vlchesnokov@bk.ru>
2022-03-10 09:42:38 +0300

    added services

---
+++ src/main/java/ru/vlchesnokov/story/service/CartService.java
@@ -0,0 +1,57 @@
+package ru.vlchesnokov.story.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.vlchesnokov.story.model.dto.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+/**
+ * Сервис по работе c корзиной товаров
+ *
+ * @author Chesnokov-vl
+ */
+@Service
+public class CartService {
+
+    @Autowired
+    private ProductInfoService productInfoService;
+
+    private final Map<Integer, Float> cache = new ConcurrentHashMap<>();
+
+    /**
+     * Возвращает расчитанную корзину товаров.
+     *
+     * @param cart корзина товаров
+     * @return расчитанная корзина товаров
+     */
+    public CalculatedCart getCalculatedCart(Cart cart) {
+        List<Integer> idsWithoutPrice = cart.getPositions().stream().map(Position::getProductId).filter(cache::containsKey).collect(Collectors.toList());
+
+        if (!idsWithoutPrice.isEmpty()) {
+            fillCache(idsWithoutPrice);
+        }
+
+        float totalAmount = 0;
+        List<AmountPosition> amountPositions = new ArrayList<>();
+
+        for (Position position : cart.getPositions()) {
+            float amount = position.getCount() * cache.get(position.getProductId());
+            totalAmount += amount;
+            amountPositions.add(new AmountPosition(position.getProductId(), position.getCount(), amount));
+        }
+
+        return new CalculatedCart(amountPositions, totalAmount);
+    }
+
+
+    private void fillCache(List<Integer> idsWithoutPrice) {
+        ProductPrice price = productInfoService.getProductPrices(idsWithoutPrice);
+        cache.putAll(price.getPrice());
+    }
+}
---
+++ src/main/java/ru/vlchesnokov/story/service/ProductInfoService.java
@@ -0,0 +1,44 @@
+package ru.vlchesnokov.story.service;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import ru.vlchesnokov.story.model.dto.ProductPrice;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.OK;
+
+/**
+ * Сервис для предоставления информации о товаре.
+ */
+@Service
+public class ProductInfoService {
+    private static final String URL_PRICE_SOURCE = "https://price-source.ru";
+
+    /**
+     * Возвращает цены товаров.
+     *
+     * @param ids идентификаторы запрашиваемых товаров
+     * @return цены товаров
+     */
+    // Вызывается внешний микросервис для предоставления информации по ценам продуктов, заданных идентификаторами
+    ProductPrice getProductPrices(List<Integer> ids) {
+        if (ids == null || ids.isEmpty()) {
+            throw new IllegalArgumentException("Не заданы идентификаторы товаров");
+        }
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<List<Integer>> requestBody = new HttpEntity<>(ids);
+
+        ResponseEntity<ProductPrice> result = restTemplate.postForEntity(URL_PRICE_SOURCE, requestBody, ProductPrice.class);
+
+        if (result.getStatusCode() == OK) {
+            return result.getBody();
+        } else {
+            throw new IllegalStateException(result.getStatusCode().getReasonPhrase());
+        }
+    }
+
+}
